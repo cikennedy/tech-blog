cab58e7bf3815140fe8f36a982adabc2bc321f09
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-10 13:35:00 -0500

    updated homeRoutes to show one post if chosen, also updated associated handlebars file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -48,6 +48,64 @@ router.get('/', (req, res) => {
     });
 });
 
+// Get one blog post by id
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_content',
+            'created_at'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'comment_content',
+                    'post_id',
+                    'user_id',
+                    'created_at'
+                ],
+                include: {
+                    model: User,
+                    attributes: [
+                        'username'
+                    ]
+                }
+            },
+            {
+                model: User,
+                attributes: [
+                    'username'
+                ]
+            }
+        ]
+    })
+
+    // Return error if there is no blog post with the id given 
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No blog post found with the given id'});
+            return;
+        }
+
+        // Use id-post handlebars template 
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('id-post', {
+            posts,
+            loggedIn: req.session.logedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 // User goes to the login/signup page if not logged in
 router.get('/login', (req, res) => {
     // If a session exists, redirect the request to the homepage
