b6a5903a6e6afe96e42e8c2226f21c33db39c1b7
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-10 09:17:35 -0500

    added code to get blog posts on homepage, needs testing once other routes are in place

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -37,4 +37,17 @@ router.get('/', (req, res) => {
             }
         ]
     })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post.get({ plain: true }));
+        res.render('homepage', {
+            posts,
+            loggedIn: req.session.logedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+    
 module.exports = router;
