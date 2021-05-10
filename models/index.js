4132de4a062ade1ba57f72a86ca50e42b1f421e9
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-09 19:18:55 -0500

    Added relationships in index model

--- models/index.js
+++ models/index.js
@@ -1,3 +1,46 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-module.exports = { User };
+// Users can have many posts
+// When a user is deleted, the associated posts are also deleted
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Users can have many comments
+// When a user is deleted, the associated comments are also deleted
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+// Posts belong to users 
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Comments belong to users 
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Comments belong to users
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+});
+
+// Comments belong to posts
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+});
+
+// Posts can have many comments
+// When a post is deleted, the associated comments are also deleted
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE',
+});
+
+module.exports = { User, Post, Comment };
