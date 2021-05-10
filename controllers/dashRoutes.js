459e4490ab42a76ae7a31ddf6390622c6bcb9a80
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-10 09:29:50 -0500

    changed login routes to the homepage instead of dashboard

--- controllers/dashRoutes.js
+++ controllers/dashRoutes.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// Prevent non logged in users from viewing the homepage
-router.get('/', withAuth, async (req, res) => {
+// Prevent non logged in users from viewing the dashboard
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
       attributes: { exclude: ['password'] },
@@ -12,7 +12,7 @@ router.get('/', withAuth, async (req, res) => {
 
     const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render('dashboard', {
       users,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
@@ -22,14 +22,6 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/login', (req, res) => {
-  // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
 
-  res.render('login');
-});
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -49,5 +49,15 @@ router.get('/', (req, res) => {
         res.status(500).json(err);
     });
 });
-    
+
+router.get('/login', (req, res) => {
+    // If a session exists, redirect the request to the homepage
+    if (req.session.logged_in) {
+      res.redirect('/');
+      return;
+    }
+  
+    res.render('login');
+  });
+
 module.exports = router;
