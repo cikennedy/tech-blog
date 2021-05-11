1fb2e8c91a9e68998c9748f8d6daaf7361e332ea
Chris Kennedy <kennedy.christopherian@gmail.com>
2021-05-10 21:34:06 -0500

    updated userRoutes with full log in, log out, sign up functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -83,70 +83,35 @@ router.post('/', (req, res) => {
     }
 );
 
-// // User login 
-// router.post('/login', (req, res) => {
-//     User.findOne({
-//         where: {
-//             email: req.body.email
-//         }
-//     }).then(dbUserData => {
-//         if(!dbUserData) {
-//             res.status(404).json({ message: 'No user found with the given id.'});
-//             return;
-//         }
+// User login 
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(404).json({ message: 'No user found with the given id.'});
+            return;
+        }
        
-//         const correctPassword = dbUserData.checkPassword(req.body.password);
+        const correctPassword = dbUserData.checkPassword(req.body.password);
 
-//         if (!correctPassword) {
-//             res.status(400).json({ message: 'Incorrect Password' })
-//             return;
-//         }
+        if (!correctPassword) {
+            res.status(400).json({ message: 'Incorrect Password' })
+            return;
+        }
 
-//         req.session.save(() => {
-//                 req.session.user_id = dbUserData.id;
-//                 req.session.email = dbUserData.email;
-//                 req.session.username = dbUserData.username;
-//                 req.session.logged_in = true;
+        req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.email = dbUserData.email;
+                req.session.username = dbUserData.username;
+                req.session.logged_in = true;
 
-//                 res.json({ user: dbUserData, message: "You have been logged in." });
-//         });
-//     });
-// });
-
-router.post('/login', async (req, res) => {
-    try {
-      // Find the user who matches the posted e-mail address
-      const userData = await User.findOne({ where: { email: req.body.email } });
-  
-      if (!userData) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      // Verify the posted password with the password store in the database
-      const validPassword = await userData.checkPassword(req.body.password);
-  
-      if (!validPassword) {
-        res
-          .status(400)
-          .json({ message: 'Incorrect email or password, please try again' });
-        return;
-      }
-  
-      // Create session variables based on the logged in user
-      req.session.save(() => {
-        req.session.user_id = userData.id;
-        req.session.logged_in = true;
-        
-        res.json({ user: userData, message: 'You are now logged in!' });
-      });
-  
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+                res.json({ user: dbUserData, message: "You have been logged in." });
+        });
+    });
+});
 
 // User logout. This is post as listed in the logout.js file 
 router.post('/logout', (req, res) => {
